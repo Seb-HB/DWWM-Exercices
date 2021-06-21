5f12fdbdddca0ef709da542646dda6cec192b852
Seb-HB <sfouvet@stagiaire-humanbooster.com>
2021-06-21 17:02:31 +0200

    update meteo

--- JS/meteoJson/public/meteo.js
+++ JS/meteoJson/public/meteo.js
@@ -1,9 +1,8 @@
-let meteoRequest = new XMLHttpRequest();
+let meteoUrl, meteoIco,meteoText,meteoDate;
+//création de l'objet carte via l'API de https://leafletjs.com/
 let meteomap = L.map('meteomap').setView([45.439695, 4.3871779], 13);
-let meteoUrl;
 
-
-
+//remplissage de la carte grace à l'API https://account.mapbox.com/access-tokens
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -14,45 +13,78 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(meteomap);
 
 
-// meteomap.onclick=function(e) {
-//     latLong = e.latlng;
-//     console.log(e);
-//     console.log(latLong);
-// }
-
+// au click sur la carte, on recupere les coordonnées et on lance la fonction getMeteo avec
 function onMapClick(e) {
     latLong = e.latlng;
-    // console.log(e);
-    // console.log(latLong);
     meteoUrl=`https://www.prevision-meteo.ch/services/json/lat=${latLong.lat}lng=${latLong.lng}`;
     console.log(meteoUrl);
-
+    let marker = L.marker([latLong.lat, latLong.lng]).addTo(meteomap);
+    getMeteo(meteoUrl);
 }
-
 meteomap.on('click', onMapClick);
 
 
-
-// document.querySelector('#meteoparam').submit=function(){
-
-// }
-
-// const getMeteo=function(url){
-//     meteoRequest.onreadystate=meteoResult(){
-//         if (this.readyState==4){
-//             (this.status == 200) ? reqSucces(this.response) : reqError(this.status, this.statusText);
-//         }else{
-//             reqError(this.status, this.statusText);
-//         }
-//     }
+function getMeteo(url){
+    let meteoRequest = new XMLHttpRequest();
+    // on surveille les changements de statuts
+    meteoRequest.addEventListener("readystatechange", function(){
+        if (this.readyState==4){
+            console.log('dedans1');
+            (this.status == 200) ? reqSuccess(this.response) : reqError(this.status, this.statusText);
+        }else{
+            console.log('dedans2');
+            reqError(this.status, this.statusText);
+        }
+    });
+    // meteoRequest.onreadystatechange= function(){
+    //     if (this.readyState==4){
+    //         (this.status == 200) ? reqSuccess(this.response) : reqError(this.status, this.statusText);
+    //     }else{
+    //         reqError(this.status, this.statusText);
+    //     }
+    // }
     
-//     meteoRequest.open("GET", url);
+    //on lance la requete
+    meteoRequest.open("GET", url);
+    meteoRequest.send();
+}
 
+//traitement du résultat quand tout est OK
+function reqSuccess(response){
+    const meteo=JSON.parse(response);
+    console.log(meteo);
+    let counter=1;
+    let meteoParent;
 
-// }
+    for (prop in meteo){
+        console.log(meteo[prop])
+        if (meteo[prop].day_long){
+            console.log(counter);
+            meteoDate=document.createElement('p');
+            meteoDate.innerHTML=meteo[prop].day_long + ' '+ meteo[prop].date;
+            meteoIco=document.createElement('img');
+            meteoIco.src=meteo[prop].icon_big;
+            meteoText=document.createElement('p');
+            meteoText.innerHTML=meteo[prop].condition;
+            meteoTMin=document.createElement('p');
+            meteoTMin.innerHTML='<img src="img/temperatureMin.png" alt="icone temperature min">'+meteo[prop].tmin;
+            meteoTMax=document.createElement('p');
+            meteoTMax.innerHTML='<img src="img/temperatureMax.png" alt="icone temperature max">'+meteo[prop].tmax;
+            meteoParent=document.querySelector(`.j${counter}`);
+            while (meteoParent.hasChildNodes()){
+                meteoParent.removeChild(meteoParent.firstChild);
+            }
+            console.log(`le parent .j${counter} est :` , meteoParent);
+            document.querySelector(`.j${counter}`).appendChild(meteoDate);
+            document.querySelector(`.j${counter}`).appendChild(meteoIco);
+            document.querySelector(`.j${counter}`).appendChild(meteoText);
+            document.querySelector(`.j${counter}`).appendChild(meteoTMin);
+            document.querySelector(`.j${counter}`).appendChild(meteoTMax);
+            counter++;
+        }
+    };
+}
 
-
-// function reqSuccess(response){
-//     const meteo=JSON.parse(response);
-//     console.log(response);
-// }
+function reqError(statut, textError){
+    console.log(statut, textError);
+}
